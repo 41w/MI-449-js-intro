ef8dc43fd8410b85b3770007de17ff9dcba74236
41w <xusiyi@msu.edu>
2019-02-07 15:58:47 -0500

    Use JavaScript to build more interactive webpages

---
+++ script.js
@@ -0,0 +1,70 @@
+var buttonElement1 = document.getElementById('button1')
+var boom = new Audio("boom.wav")
+buttonElement1.addEventListener('click', function () {
+  console.log('Boom was clicked!')
+  boom.play();
+})
+
+
+var buttonElement2 = document.getElementById('button2')
+var clap = new Audio("clap.wav")
+buttonElement2.addEventListener('click', function () {
+  console.log('Clap was clicked!')
+  clap.play();
+})
+
+
+var buttonElement3 = document.getElementById('button3')
+var hihat = new Audio("hihat.wav")
+buttonElement3.addEventListener('click', function () {
+  console.log('Hihat was clicked!')
+  hihat.play();
+})
+
+
+var buttonElement4 = document.getElementById('button4')
+var kick = new Audio("kick.wav")
+buttonElement4.addEventListener('click', function () {
+  console.log('Kick was clicked!')
+  kick.play();
+})
+
+
+var buttonElement5 = document.getElementById('button5')
+var openhat = new Audio("openhat.wav")
+buttonElement5.addEventListener('click', function () {
+  console.log('Openhat was clicked!')
+  openhat.play();
+})
+
+
+var buttonElement6 = document.getElementById('button6')
+var ride = new Audio("ride.wav")
+buttonElement6.addEventListener('click', function () {
+  console.log('Ride was clicked!')
+  ride.play();
+})
+
+
+var buttonElement7 = document.getElementById('button7')
+var snare = new Audio("snare.wav")
+buttonElement7.addEventListener('click', function () {
+  console.log('Snare was clicked!')
+  snare.play();
+})
+
+
+var buttonElement8 = document.getElementById('button8')
+var tink = new Audio("tink.wav")
+buttonElement8.addEventListener('click', function () {
+  console.log('Tink was clicked!')
+  tink.play();
+})
+
+
+var buttonElement9 = document.getElementById('button9')
+var tom = new Audio("tom.wav")
+buttonElement9.addEventListener('click', function () {
+  console.log('Tom was clicked!')
+  tom.play();
+})
